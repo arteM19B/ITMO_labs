6dd4ac9d943ea2897434a7898109d9c96aa8034b
arteM19B <159503211+arteM19B@users.noreply.github.com>
2025-09-25 10:10:57 +0300

    Update Lab1.java

--- Lab1/Lab1.java
+++ Lab1/Lab1.java
@@ -1,8 +1,8 @@
-import static java.lang.Math. *;
+import static java.lang.Math.*;
 
 // 26442
 public class Lab1 {
-    public static void main (String [] args) {
+    public static void main(String[] args) {
         short a[] = new short[9];
         for (int i = 0; i <= 8; i++) {
             short j = 22;
@@ -18,22 +18,30 @@ public class Lab1 {
         double w[][] = new double[9][13];
         for (int i = 0; i < 9; i++) {
             for (int j = 0; j < 13; j++) {
-                if (a[i] == 8) {
-                    w[i][j] = pow(E, cos(pow(x[j], x[j] / 0.5)));
-                } else if (a[i] == 10 || a[i] == 14 || a[i] == 16 || a[i] == 18) {
-                    w[i][j] = log(pow(sin(pow(pow(((x[j] - 1) / 2) / 3, 2), 0.5)), 2));
-                } else {
-                    w[i][j] = log(sqrt(pow(pow(abs(x[j]), 2) + 1, 2)));
-                }
+                w[i][j] = calculateElement(a[i], x[j]);
             }
         }
-        for(int i = 0; i <  9; i++) {
-	        for(int j = 0; j <  13; j++) {
-		        String str = String.format("%7.4f ", w[i][j]);
-		        System.out.print(str);
-	        }
-	        System.out.println();
-	    }
-
+        
+        printMatrix(w);
+    }
+    
+    public static double calculateElement(short aValue, double xValue) {
+        if (aValue == 8) {
+            return pow(E, cos(pow(xValue, xValue / 0.5)));
+        } else if (aValue == 10 || aValue == 14 || aValue == 16 || aValue == 18) {
+            return log(pow(sin(pow(pow(((xValue - 1) / 2) / 3, 2), 0.5)), 2));
+        } else {
+            return log(sqrt(pow(pow(abs(xValue), 2) + 1, 2)));
+        }
+    }
+    
+    public static void printMatrix(double[][] matrix) {
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                String str = String.format("%7.4f ", matrix[i][j]);
+                System.out.print(str);
+            }
+            System.out.println();
+        }
     }
 }
